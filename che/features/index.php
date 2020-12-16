f285accd97978920d2ff0ad2bec92b28e3e0bb5a
Florent Benoit <fbenoit@redhat.com>
2020-12-16 13:30:16 +0100

    RHDEVDOCS-1045: Perform language/consistency cleanup

--- che/features/index.php
+++ che/features/index.php
@@ -46,15 +46,13 @@
   <div class="row" id="prod-runtimes">
     <div class="col-sm-6">
       <h2>Production Runtimes</h2>
-      <p>Eclipse Che works with any container, even multi-container runtimes. Run your workspace on Kubernetes, OpenShift or Docker. Use an image from DockerHub, your own private registry or one of the many that ship with Che. Add agents for SSH, terminal and language services to dev-enable your production images. When stopped, Che workspaces your project and key dependency state is persisted between runs.</p>
+      <p>Eclipse Che works with any container, even multi-container runtimes. Run your workspace on Kubernetes and OpenShift. Use an image from DockerHub, Quay.io, your own private registry or one of the many that ship with Che. Add terminal and language services to dev-enable your production images.</p>
       <p>Under the hood:</p>
       <ul>
-        <li>Container Runtimes</li>
-        <li>Compose Runtimes</li>
-        <li>Root Access Terminal</li>
-        <li>SSH Access</li>
-        <li>Pre-Built and Custom Stacks</li>
-        <li>Workspace Snapshots</li>
+        <li>User Container Runtimes</li>
+        <li>Sidecar Tooling Container Runtimes</li>
+        <li>Terminal</li>
+        <li>Pre-Built and Custom stacks</li>
       </ul>
       <p><a href="#top">Back to Top</a></p>
     </div>
@@ -68,16 +66,15 @@
   <div class="row" id="dev-mode">
     <div class="col-sm-6">
       <h2>“Dev Mode” your Workspace</h2>
-      <p>Inject developer services into a workspace with agents for syntax auto-complete, error checking and a debugger. Add intellisense for additional languages with language server agents. Enable root-access terminal and SSH access with the flick of a switch.</p>
+      <p>Inject developer services into a workspace with sidecars for syntax auto-complete, error checking and a debugger. Add intellisense for additional languages with language servers. Enable terminal access with the flick of a switch.</p>
       <p>Under the hood:</p>
       <ul>
         <li>Language Servers</li>
         <li>Intellisense and Refactoring</li>
         <li>Debuggers</li>
-        <li>Workspace Agents</li>
+        <li>Sidecar containers for the tooling</li>
         <li>Intelligent Commands</li>
-        <li>Root Access Terminal</li>
-        <li>SSH Access</li>
+        <li>Access Terminal</li>
       </ul>
       <p><a href="#top">Back to Top</a></p>
     </div>
@@ -94,7 +91,8 @@
       <p>Under the hood:</p>
       <ul>
         <li>Browser IDE</li>
-        <li>RESTful Workspace APIs</li>
+        <li>Eclipse IDE</li>
+        <li>Intellij</li>
       </ul>
       <p><a href="#top">Back to Top</a></p>
     </div>
@@ -111,7 +109,6 @@
       <p>Under the hood:</p>
       <ul>
         <li>Runtime Stacks</li>
-        <li>Team Workspaces</li>
         <li>Project Samples</li>
       </ul>
       <p><a href="#top">Back to Top</a></p>
@@ -125,7 +122,7 @@
   <div class="row" id="lsp">
     <div class="col-sm-6">
       <h2>Language Server Protocol</h2>
-      <p>The <a href="https://github.com/Microsoft/language-server-protocol" alt="LSP on GitHub">Language Server Protocol</a> was developed by Microsoft, Codenvy, Red Hat and IBM as a common protocol to provide language services including syntax analysis, outlining, and refactoring within the Eclipse Che, or the IDE or editor of their own choice. The protocol can be used between client tools and language servers to integrate features such as auto complete, goto definition, and find all references. Language server is a term being applied to the language intelligence programs that are providing the services.</p>
+      <p>The <a href="https://github.com/Microsoft/language-server-protocol" alt="LSP on GitHub">Language Server Protocol</a> was developed by Microsoft, Red Hat and IBM as a common protocol to provide language services including syntax analysis, outlining, and refactoring within the Eclipse Che, or the IDE or editor of their own choice. The protocol can be used between client tools and language servers to integrate features such as auto complete, goto definition, and find all references. Language server is a term being applied to the language intelligence programs that are providing the services.</p>
       <p><a href="#top">Back to Top</a></p>
     </div>
 
@@ -150,8 +147,8 @@
 
   <div class="row" id="ssh">
     <div class="col-sm-6">
-      <h2>SSH / Terminal</h2>
-      <p>Che injects an SSH daemon into workspace machines. Connect your existing IDE - Eclipse if you like - with an SSH sync point. Upload your key pair or have Che generate a new pair for each workspace. You can also access your workspace with Che's browser terminal, giving you root access to workspace machines.</p>
+      <h2>Terminal</h2>
+      <p>Che enables terminal access to all containers of the workspace. Connect your existing IDE - Eclipse if you like - remotely by syncing /projects folder of your workspace.</p>
       <p><a href="#top">Back to Top</a></p>
     </div>
 
@@ -164,7 +161,7 @@
   <div class="row" id="stacks">
     <div class="col-sm-6">
       <h2>Stacks</h2>
-      <p>A stack is a runtime configuration for a workspace. It contains a runtime recipe, meta information like tags, description, environment name, and security policies. Stacks are displayed within the user dashboard and stack tags are used to filter the project code samples that are available. A stack's recipe can be Kubernetes YAML, a Dockerfile or a Docker Compose file that will create a runtime to be embedded into the workspace. You can use any of Che's 30+ built-in stacks or author your own unique stack for your work.</p>
+      <p>A stack is a runtime configuration for a workspace represented by a devfile. It contains components (runtime), projects and commands.</p>
       <p><a href="#top">Back to Top</a></p>
     </div>
 
@@ -177,7 +174,7 @@
   <div class="row" id="restful-workspaces">
     <div class="col-sm-6">
       <h2>RESTful Workspaces</h2>
-      <p>All Che services are reachable through RESTful APIs. Che exposes APIs for managing the workspace master, and also provides RESTful access to each individual workplace through the workspace agent. The Che workspace master provides workspace orchestration and user management. In each workspace agent is a micro Che server that exposes project-specific APIs. For example, Che has RESTful JDT wrappers to enable 100+ forms of distributed Java intellisense. This Java-specific API is injected as a workspace agent by Che. Programming the API is simple - get started by <a href="https://www.eclipse.org/che/docs/server/rest-api/index.html">browsing our APIs with Swagger</a>.</p>
+      <p>All Che services are reachable through RESTful APIs. Che exposes APIs for managing the workspace server. The Che server provides workspace orchestration and user management.</p>
       <p><a href="#top">Back to Top</a></p>
     </div>
 
@@ -190,7 +187,7 @@
     <div class="col-sm-6">
       <h2>Cloud IDE</h2>
       <p>A no-installation browser IDE and IOE accessible from any local or remote device. Thin, fast, and beautiful - it's the IDE our own engineers wanted. The IDE is packaged as cross-browser JavaScript and CSS, hosted as resources cached by browsers. Work on multiple workspaces in different browser tabs with each tab consuming ~100MB of RAM. It's a smoother interactive experience that doesn't suffer blockages from thrashing when the workspace is remote.</p>
-      <p>The tools you expect are there: embedded Orion editor, numerous key bindings, globalized keyboard support, and git / subversion tools including diff.</p>
+      <p>The tools you expect are there: Eclipse Theia editor, numerous key bindings, globalized keyboard support, and git tooling.</p>
       <p><a href="#top">Back to Top</a></p>
     </div>
 
@@ -203,7 +200,7 @@
   <div class="row" id="multi-project">
     <div class="col-sm-6">
       <h2>Multi-Project</h2>
-      <p>Che is a true IDE platform, recognizing projects are bound to a repository and given a type. Project types bestow special behaviors, such as when Che injects the RESTful JDT core into workspace machines when Java projects are added. Workspaces can have multiple projects, each with their own type. Projects can be independently built and run, even though all projects share a common workspace machine. Currently, Che has limited project types for JavaScript, Maven, and ‘blank’. <a href="https://github.com/eclipse/che/blob/master/CONTRIBUTING.md">We'll collaborate with the ecosystem</a> to package new types for frameworks, packaging systems and programming languages.</p>
+      <p>Che is a true IDE platform. Projects can be independently built and run, even though all projects share a common workspace container. Che has support for numerous languages by using VS Code extensions and the language server protocol.</p>
       <p><a href="#top">Back to Top</a></p>
     </div>
 
@@ -216,7 +213,7 @@
   <div class="row" id="commands">
     <div class="col-sm-6">
       <h2>Commands</h2>
-      <p>A command is a process that is injected into a machine. Commands are provided by users, workspaces, or projects. Commands have a type, like projects, which imbue additional behaviors. For example, the maven command type has inherent knowledge of how maven lifecycle phases operate. When executed, a command is translated into a process and injected into a machine, where it can operate against projects or other resources within the machine itself. Commands are context sensitive, allowing a user to execute commands across projects and modules, getting context-specific outcomes.</p>
+      <p>A command is a process that is executed into a container. Commands are provided by workspaces or plugins. When executed, a command is translated into a task and executed into a container, where it can operate against projects or other resources within the container itself. Commands are context sensitive, allowing a user to execute commands across projects and modules, getting context-specific outcomes.</p>
       <p><a href="#top">Back to Top</a></p>
     </div>
 
@@ -274,8 +271,8 @@
   <div class="row" id="open-source">
     <div class="col-sm-6">
       <h2>Open Source to the Core</h2>
-      <p>Eclipse Che has been developed over 4 years with contributions from Codenvy, eXo Platform, Red Hat, Salesforce, IBM, SAP, Microsoft, Intuit, WSO2, Serli, and open source individuals from China, Brazil, France, Ukraine, Russia, Canada, India, Sri Lanka, and the United States. It is part of the Eclipse Cloud Development top-level project, and ecosystem contributions are open and encouraged. </p>
-      <p>Please participate, even if only spiritually. There are many ways to get involved including starring the <a href="https://github.com/codenvy/che/stargazers">GitHub repo</a>, <a href="https://github.com/codenvy/che/issues">submitting issues</a>, <a href="https://github.com/eclipse/che-docs">writing docs</a>, or <a href="https://www.eclipse.org/che/docs/plugins/introduction/index.html">contributing plug-ins</a>.</p>
+      <p>Eclipse Che has been developed over years with contributions from Codenvy, eXo Platform, Red Hat, Salesforce, IBM, SAP, Microsoft, Intuit, WSO2, Serli, and open source individuals from China, Brazil, France, Ukraine, Russia, Canada, India, Sri Lanka, and the United States. It is part of the Eclipse Cloud Development top-level project, and ecosystem contributions are open and encouraged. </p>
+      <p>Please participate, even if only spiritually. There are many ways to get involved including starring the <a href="https://github.com/eclipse/che/stargazers">GitHub repo</a>, <a href="https://github.com/eclipse/che/issues">submitting issues</a>, <a href="https://github.com/eclipse/che-docs">writing docs</a>, or <a href="https://www.eclipse.org/che/docs/che-7/contributor-guide/developing-che-theia-plug-ins/">contributing plug-ins</a>.</p>
       <p><a href="#top">Back to Top</a></p>
     </div>
 
