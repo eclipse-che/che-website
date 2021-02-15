e4827d742ad4935225ada03fc1a8ec2390ca54db
Ilya Buziuk <ibuziuk@redhat.com>
2021-02-15 17:33:31 +0100

    Adding Hosted Che EOL warning
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- che/index.php
+++ che/index.php
@@ -33,6 +33,8 @@
 <hr class="gray">
 <div class="container-fluid content start-working">
 <h3>Try Eclipse Che using the online service hosted by Red Hat</h3>
+<blockquote><a href="https://che.openshift.io/">Eclipse Che hosted by Red Hat</a> is going to be shut down on <strong>March 1, 2021 at 00:01 GMT</strong>. A new service based on <a href='https://developers.redhat.com/developer-sandbox#assembly-field-sections-59571'>Red Hat CodeReady Workspaces</a> is available now. Details about the new service, including signing up for it, can be found <a href='https://developers.redhat.com/developer-sandbox'>here</a>.</blockquote>
+
   <div class="row">
     <div class="col-sm-4 col-md-3">
         <div class="border media">
