6cf1647fc32a39c0e7a74d4f0e12d3bd4cae104d
Ilya Buziuk <ibuziuk@redhat.com>
2021-02-16 17:20:30 +0100

    Improving Hosted Che EOL message
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- che/getting-started/cloud/index.php
+++ che/getting-started/cloud/index.php
@@ -19,7 +19,7 @@
   <div class="container-fluid">
     <h1>Eclipse Che hosted by Red Hat</h1>
     <h3>Self-service Eclipse Che developer workspaces hosted at che.openshift.io</h3>
-    <blockquote style="border-style:solid;border-color:red"><a href="https://che.openshift.io/">Eclipse Che hosted by Red Hat</a> is going to be shut down on <strong>March 1, 2021 at 00:01 GMT</strong>. A new service based on <a href='https://developers.redhat.com/developer-sandbox#assembly-field-sections-59571'>Red Hat CodeReady Workspaces</a> is available now. Details about the new service, including signing up for it, can be found <a href='https://developers.redhat.com/developer-sandbox'>here</a>.</blockquote>
+    <blockquote style="border-style:solid;border-color:red"><a href="https://che.openshift.io/">Eclipse Che hosted by Red Hat</a> is going to be shut down on <strong>March 1, 2021 at 00:01 GMT</strong>. A new service based on <a href='https://developers.redhat.com/developer-sandbox#assembly-field-sections-59571'>Red Hat CodeReady Workspaces</a> is available now. <a href='https://developers.redhat.com/developer-sandbox'>Developer Sandbox for Red Hat OpenShift</a> provides more details about the new service.</blockquote>
   </div> <!-- /.container-fluid -->
 </div> <!-- /.jumbotron -->
 
